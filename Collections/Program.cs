52b4878f92108819248c9f709b33c96e6fed75e6
Bayrktar <bayrktarkadir@gmail.com>
2022-09-09 17:47:59 +0300

    09.09.2022

---
+++ Collections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Abdülkadir", "Selahaddin", "Hava", "Abdullah" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];        //burada, yukarıdaki diziye yeni bir eleman eklenmedi. 5 elemanlı yeni bir dizi oluşturuldu. Aşğıdaki listeye ekleme gibi bir özellik yok
+            //isimler[4] = "Tomris";          //burada da oluştural boş dizinin 5. elemanı atandı
+            //Console.WriteLine(isimler[4]);  //burada ekrana Tomris yazdırılır
+            //Console.WriteLine(isimler[0]);  // fakata burada ekrana bir şey yazdırılmaz çünkü dizinin o elemanı boş
+
+
+            List<string> isimler2 = new List<string> {"Abdülkadir", "Selahaddin", "Hava", "Abdullah"};
+
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+            isimler2.Add("Tomris");             //burada, yukarıda oluşturulan listeye yeni bir eleman eklendi
+            Console.WriteLine(isimler2[4]);     //oluşturulan yeni eleman ekrana yazdırıldı
+            Console.WriteLine(isimler2[0]);     //listenin zaten elemanı olan Abdülkadir yazdırıldı
+
+        }
+    }
+}
---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    class MyList<T>                                         // Type(tip)in T^sini verdik. String verirsek sadece string, int verirsek sadece int değeri alırdı//arka planda sürekli dizi tutmyaı sağlayan bir generic
+    {
+        T[] items;
+        public MyList()                                     //class her newlendiğinde arkaplanda 0 elemanlı bir dizi oluşturuyor
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;                          //diziyi newleyip eleman eklemeden önce; dizideki elemanlar kaybolmasın diye geçici diziye (tempArray) tutturduk
+            items = new T[items.Length + 1];                //diziye her yeni eleman eklendiğinde dizinin eleman sayısını 1 arttırıyor
+            for (int i = 0; i < tempArray.Length; i++)      //geçici diziye tutturulan elemanlar asıl diziye aktarıldı.
+            {
+                items[i] = tempArray[i];
+            }
+
+            items[items.Length-1] = item;                   //dizinin eleman sayısı-1 o dizinin son elemanını verir. Add ile oluşan boşluğa da son eleman olan item eklenir
+        }
+    }
+}
